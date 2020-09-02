336d41f3a4b8b7e9510c7a9fade8f32bbf199d97
Alexandre B <alexandre.bobkov@gmail.com>
2020-09-02 00:37:22 -0400

    widget

---
+++ app/src/main/java/ca/dev/activcountwebapp/activcount_widget.java
@@ -0,0 +1,48 @@
+package ca.dev.activcountwebapp;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class activcount_widget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText_001 = context.getString(R.string.business_name_en);
+        CharSequence widgetText_002 = context.getString(R.string.contacts);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.activcount_widget);
+        views.setTextViewText(R.id.appwidget_text_001, widgetText_001);
+        views.setTextViewText(R.id.appwidget_text_002, widgetText_002);
+
+        //views.setImageViewBitmap(R.id.imageView);
+        //ImageView logo_view = (ImageView)R.id.imageView;
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
