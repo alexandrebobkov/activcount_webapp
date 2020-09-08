a3d96933cc10c7c19c7998914a062d9fc5cdf1ff
Alexandre B <alexandre.bobkov@gmail.com>
2020-09-08 03:00:38 -0400

    transperency. date & time. formatting.

--- app/src/main/java/ca/dev/activcountwebapp/activcount_widget.java
+++ app/src/main/java/ca/dev/activcountwebapp/activcount_widget.java
@@ -3,12 +3,39 @@ package ca.dev.activcountwebapp;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.widget.RemoteViews;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class activcount_widget extends AppWidgetProvider {
+
+    public static Bitmap BuildUpdate (String txt_time, float size, Context context) {
+        Paint paint = new Paint();
+        paint.setTextSize(size);
+        Typeface ourCustomTypeface = Typeface.createFromAsset(context.getAssets(), "fonts/Comfortaa-Regular.ttf");
+        paint.setTypeface(ourCustomTypeface);
+        paint.setColor(Color.parseColor("#3A9F44"));
+        paint.setTextAlign(Paint.Align.LEFT);
+        paint.setSubpixelText(true);
+        paint.setAntiAlias(true);
+        float baseline = -paint.ascent();
+        int width = (int) (paint.measureText(txt_time) + 0.5f);
+        int height = (int) (baseline+paint.descent() + 0.5f);
+        Bitmap image = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_4444);
+        //Bitmap image = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+        Canvas canvas = new Canvas(image);
+        canvas.drawText(txt_time, 0, baseline, paint);
+        return image;
+    }
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
@@ -17,8 +44,16 @@ public class activcount_widget extends AppWidgetProvider {
         CharSequence widgetText_002 = context.getString(R.string.contact_phone);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.activcount_widget);
-        views.setTextViewText(R.id.appwidget_text_001, widgetText_001);
+        //views.setTextViewText(R.id.appwidget_text_001, widgetText_001);
         views.setTextViewText(R.id.appwidget_text_002, widgetText_002);
+
+        SimpleDateFormat date_format = new SimpleDateFormat("dd MMM, yyyy");
+        SimpleDateFormat time_format = new SimpleDateFormat("HH : MM");
+
+        //views.setImageViewBitmap(R.id.img_time, BuildUpdate("20:25", 100f, context));
+        views.setImageViewBitmap(R.id.img_date, BuildUpdate(date_format.format(new Date()), 80f, context));
+        views.setImageViewBitmap(R.id.img_time, BuildUpdate(time_format.format(new Date()), 100f, context));
+        views.setImageViewBitmap(R.id.img_business_name, BuildUpdate("Alexander Specialised Accounting Services", 80f, context));
 
         //views.setImageViewBitmap(R.id.imageView);
         //ImageView logo_view = (ImageView)R.id.imageView;
