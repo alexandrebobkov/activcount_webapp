e19282f4e0b9f4d3b5e8417e2332bcd1abd5a5a2
Alexandre B <alexandre.bobkov@gmail.com>
2020-09-13 14:35:51 -0400

    design

---
+++ app/src/main/java/ca/dev/activcountwebapp/Widget_5x2_001.java
@@ -0,0 +1,23 @@
+package ca.dev.activcountwebapp;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.webkit.WebView;
+import android.widget.RemoteViews;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Widget_5x2_001 extends AppWidgetProvider {
+}
