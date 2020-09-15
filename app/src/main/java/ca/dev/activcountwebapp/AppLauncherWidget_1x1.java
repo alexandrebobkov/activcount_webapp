d5457734beab26b71f5021056eb282900c7076f7
Alexandre B <alexandre.bobkov@gmail.com>
2020-09-15 00:49:52 -0400

    widget 1x1

--- app/src/main/java/ca/dev/activcountwebapp/AppLauncherWidget_1x1.java
+++ app/src/main/java/ca/dev/activcountwebapp/AppLauncherWidget_1x1.java
@@ -1,3 +1,39 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Alexandre Comptabilite Specialise Ltee
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ *
+ *  Date Created:        August 30, 2020
+ *  Last time updated:   September 7, 2020
+ *  Revision:
+ *
+ *  Author:              Alexandre Bobkov
+ *  Company:             Alexandre Comptabilite Specialise Ltee.
+ *
+ *  Program description: application with launcher widget.
+ *
+ */
+
 package ca.dev.activcountwebapp;
 
 import android.app.PendingIntent;
@@ -18,12 +54,12 @@ public class AppLauncherWidget_1x1 extends AppWidgetProvider {
 		CharSequence widgetText = context.getString(R.string.appwidget_text);
 		// Construct the RemoteViews object
 		RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.app_launcher_widget_1x1);
-		views.setTextViewText(R.id.appwidget_text, widgetText);
+		//views.setTextViewText(R.id.appwidget_logo, widgetText);
 
-		views.setImageViewResource(R.id.appwidget_text, R.mipmap.ic_logo);
+		views.setImageViewResource(R.id.appwidget_1x1_logo, R.mipmap.ic_logo);
 		Intent intentWeb = new Intent (context, MainActivity.class);
 		PendingIntent pendingWeb = PendingIntent.getActivity(context,appWidgetId,intentWeb,PendingIntent.FLAG_UPDATE_CURRENT);
-		views.setOnClickPendingIntent(R.id.widget_5x2_logo, pendingWeb);
+		views.setOnClickPendingIntent(R.id.appwidget_1x1_logo, pendingWeb);
 
 		// Instruct the widget manager to update the widget
 		appWidgetManager.updateAppWidget(appWidgetId, views);
